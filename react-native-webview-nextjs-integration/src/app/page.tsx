0d222e1e2cfe52d5b3e0e1e7dfce1df6f39c1bba
rich-hackle <richie.kwon@hackle.io>
2025-08-20 17:44:49 +0900

    remoteConfig hook

--- react-native-webview-nextjs-integration/src/app/page.tsx
+++ react-native-webview-nextjs-integration/src/app/page.tsx
@@ -8,6 +8,7 @@ import VariationTester from "./components/VariationTester";
 import FeatureTester from "./components/FeatureTester";
 import UserController from "./components/UserController";
 import CustomTracker from "./components/CustomTracker";
+import RemoteConfig from "./components/RemoteConfig";
 
 export default function Home() {
   return (
@@ -23,6 +24,12 @@ export default function Home() {
           <h2>Feature Flag [key: 22]</h2>
           <Suspense fallback={<Loader />}>
             <FeatureTester />
+          </Suspense>
+        </div>
+        <div>
+          <h2>Remote Config [key: 'targeting_rule_test']</h2>
+          <Suspense fallback={<Loader />}>
+            <RemoteConfig />
           </Suspense>
         </div>
         <UserController />
